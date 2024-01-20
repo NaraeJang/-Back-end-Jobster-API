9024c139a00624903124096a76a2eaa098137dd2
NaraeJang <j.naaaarae@gmail.com>
2024-01-20 15:29:47 -0500

    Pagination

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -38,9 +38,17 @@ const getAllJobs = async (req, res) => {
     result = result.sort("-position");
   }
 
-  const jobs = await result; // 3. keep await at the end, so that we can have divided logics.
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
-  res.status(StatusCodes.OK).json({ jobs });
+  result = result.skip(skip).limit(limit);
+
+  const jobs = await result; // 3. keep await at the end, so that we can have divided logics.
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages });
 };
 
 const getJob = async (req, res) => {
